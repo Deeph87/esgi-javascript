413c0f0d520821659b6356674d65f4acd1397ae5
Jérémy Haudry <jeremy-haudry@hotmail.fr>
2018-10-03 11:34:24 +0200

    corrections for the moulinette

--- exercice-1/string.js
+++ exercice-1/string.js
@@ -1,35 +1,35 @@
-// let test1 = "Bienvenue dans ce train basse vitesse";
+// var test1 = "Bienvenue dans ce train basse vitesse";
 
 /**
  *
  * @param string
  * @returns {string}
  */
-let ucfirst = (string) => {
+function ucfirst(string) {
     if (typeof string !== 'string') {
         return "";
     }
     return string.charAt(0).toUpperCase() + string.substring(1).toLowerCase();
-};
-// console.log(ucfirst(test1));
+}
+// console.log(ucfirst("TEST"));
 
 /**
  *
  * @param string
  * @returns {string}
  */
-let capitalize = (string) => {
+function capitalize(string) {
     if (typeof string !== 'string') {
         return "";
     }
 
-    let stringExploded = string.split(" ");
-    let ret = [];
-    for (let i in stringExploded) {
+    var stringExploded = string.split(" ");
+    var ret = [];
+    for (var i in stringExploded) {
         ret.push(ucfirst(stringExploded[i]));
     }
     return ret.join(" ");
-};
+}
 // console.log(capitalize(test1));
 
 /**
@@ -37,48 +37,48 @@ let capitalize = (string) => {
  * @param string
  * @returns {string}
  */
-let camelCase = (string) => {
+function camelCase(string) {
     if (typeof string !== 'string') {
         return "";
     }
-    let capitalizedString = capitalize(string);
-    return capitalizedString.replace(/\ +/g, "");
-};
-// console.log(camelCase(test1));
+    var capitalizedString = capitalize(string);
+    return capitalizedString.replace(/( |_)+/g, "");
+}
+// console.log(camelCase("ToggleCase is_the coolest"));
 
 /**
  *
  * @param string
  * @returns {string}
  */
-let snakeCase = (string) => {
+function snakeCase (string) {
     if (typeof string !== 'string') {
         return "";
     }
     string = string.toLowerCase();
     return string.replace(/ +/g, "_");
-};
-// console.log(snakeCase(test1));
+}
+// console.log(snakeCase("toggle case is the coolest"));
 
 /**
  *
  * @param string
  * @returns {string}
  */
-let leet = (string) => {
+function leet(string) {
     if (typeof string !== 'string') {
         return "";
     }
-    string = string.replace(/a+/gi, "4");
-    string = string.replace(/e+/gi, "3");
-    string = string.replace(/i+/gi, "1");
-    string = string.replace(/o+/gi, "0");
-    string = string.replace(/u+/gi, "(-)");
-    string = string.replace(/y+/gi, "7");
+    string = string.replace(/a{1}/gi, "4");
+    string = string.replace(/e{1}/gi, "3");
+    string = string.replace(/i{1}/gi, "1");
+    string = string.replace(/o{1}/gi, "0");
+    string = string.replace(/u{1}/gi, "(_)");
+    string = string.replace(/y{1}/gi, "7");
 
     return string
-};
-// console.log(leet(test1));
+}
+// console.log(leet("evaluation"));
 
 /**
  *
@@ -86,35 +86,36 @@ let leet = (string) => {
  * @param path
  * @returns {*}
  */
-let prop_access = (object, path) => {
-    if (path === "" || path === null) {
-        return object;
-    }
+function prop_access(object, path) {
+    if (typeof path === "string" && path.trim() !== "") {
+        var paths = path.split(".");
 
-    let pathExploded = path.split(".");
-    let tmpObject = object,
-        undefinedRet = [];
+        paths.map((p)=>{
 
-    for (let i in pathExploded) {
-        tmpObject = tmpObject[pathExploded[i]];
-        undefinedRet.push(pathExploded[i]);
+            if (object.hasOwnProperty(p)) {
+                object = object[p];
+            }
 
-        if (typeof tmpObject === "string") {
-            return tmpObject;
-        } else if (typeof tmpObject === "undefined") {
-            return undefinedRet.join(".") + " not exist"
+        });
+
+        if (typeof object === "string") {
+            return object;
+        } else {
+            return path + " not exist";
         }
     }
-};
+    return object;
 
-// let prairie = {
+}
+
+// var prairie = {
 //     animal: {
 //         type: {
 //             name: "chien"
 //         }
 //     }
 // };
-// let path = "animal.type.name";
+// var path = "animal.type.name";
 
 // console.log(prop_access(prairie, path));
 // console.log(prop_access(prairie, path));
@@ -124,42 +125,46 @@ let prop_access = (object, path) => {
  * @param string
  * @returns {string}
  */
-let verlan = (string) => {
+function verlan(string) {
     if (typeof string !== 'string') {
         return "";
     }
-    let stringArray = [];
-    for (i = 0;  i < string.length; i++){
-        stringArray[i] = string.charAt(i);
+
+    var separator = ' ',
+        stringsArray = string.split(separator);
+
+    for (var i in stringsArray){
+        stringsArray[i] = stringsArray[i].split("").reverse().join("");
     }
-    return stringArray.reverse().join('');
-};
-//console.log(verlan(test1));
+    return stringsArray.join(separator);
+}
+// console.log(verlan("yoda m Luke"));
 
 /**
  *
  * @param string
  * @returns {string}
  */
-let yoda = (string) => {
+function yoda(string) {
     if (typeof string !== 'string') {
         return "";
     }
-    let words = string.split(" ");
+    var words = string.split(" ");
     return words.reverse().join(" ");
-};
+}
 // console.log(yoda(test1));
 
-let vig = (strToCrypt, key) => {
-    let alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+function vig(strToCrypt, key) {
+    var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
     console.log(alphabet.length);
-    let vigArray = [],
-        popped, tmpAlphabet;
-    for (let i = 0; i <= alphabet.length; i++) {
+    var vigArray = [],
+        popped,
+        tmpAlphabet;
+    for (var i = 0; i <= alphabet.length; i++) {
         tmpAlphabet = alphabet;
         vigArray.push(tmpAlphabet);
         popped = alphabet.pop();
         alphabet.unshift(popped);
     }
     console.log(vigArray);
-};
+}
